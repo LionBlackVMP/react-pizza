dbb09726b2e2a7da25d6973a2f71336d1f3bae92
LionBlackVMP <LionBlack1079@gmail.com>
2024-11-09 16:39:30 +0100

    add vite svg plugin, create assets/icon for svg files, fix search input

--- src/pages/Cart/ItemCart.tsx
+++ src/pages/Cart/ItemCart.tsx
@@ -1,7 +1,10 @@
 import { useAppDispatch, useAppSelector } from "../../hooks/hooks";
-
 import { addItem, CartItem, removeItem, removeSameItems } from "../../redux/slices/cartSlice";
 import { cartSelect } from "../../redux/selectors";
+
+import Plus from "../../assets/icons/Cart/plus.svg?react";
+import Minus from "../../assets/icons/Cart/minus.svg?react";
+import Cross from "../../assets/icons/Cart/cross.svg?react";
 
 export const ItemCart = () => {
   const dispatch = useAppDispatch();
@@ -16,6 +19,7 @@ export const ItemCart = () => {
   const findItem = ({ id, size, type }: RenderedItems, items: CartItem[]) => {
     return items.find((el) => el.id === id && el.size === size && el.type === type);
   };
+
   const renderedItems = items
     .reduce((acc: RenderedItems[], item: CartItem) => {
       const existingIndex = acc.findIndex(
@@ -28,7 +32,7 @@ export const ItemCart = () => {
         acc.push({
           ...item,
           quantity: 1,
-          key: item.id + ` ${item.type}, ${item.size} cm.`,
+          key: item.id + `${item.type}, ${item.size} cm.`,
           description: `${item.type}, ${item.size} cm.`,
         });
       }
@@ -82,44 +86,14 @@ export const ItemCart = () => {
                 className="button button--outline button--circle cart__item-count-minus"
                 onClick={() => handleDecrease(item)}
               >
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  ></path>
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  ></path>
-                </svg>
+                <Minus />
               </div>
               <b>{item.quantity}</b>
               <div
                 className="button button--outline button--circle cart__item-count-plus"
                 onClick={() => handleIncrease(item, items)}
               >
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  ></path>
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  ></path>
-                </svg>
+                <Plus />
               </div>
             </div>
             <div className="cart__item-price">
@@ -127,22 +101,7 @@ export const ItemCart = () => {
             </div>
             <div className="cart__item-remove" onClick={() => handleRemove(item)}>
               <div className="button button--outline button--circle">
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  ></path>
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  ></path>
-                </svg>
+                <Cross />
               </div>
             </div>
           </div>
